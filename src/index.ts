6ef646d80945d116defd99288be1ec2cd9ab2bd1
monkey-king-jim <57361690+monkey-king-jim@users.noreply.github.com>
2022-03-06 17:27:54 -0500

    URLencoded

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 require("dotenv").config();
 import express, { Request, Response } from "express";
 import axios from "axios";
+import url from "url";
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,18 +11,29 @@ app.get("/api/auth/discord/redirect", async (req: Request, res: Response) => {
   console.log(req.query);
   const { code } = req.query;
   if (code) {
-    const response = await axios.post(
-      "https://discord.com/api/v8/oauth2/token",
-      {
-        client_id: process.env.DISCORD_OAUTH_CLIENT_ID,
-        client_secret: process.env.DISCORD_OAUTH_CLIENT_SECRET,
+    try {
+      const formData = new url.URLSearchParams({
+        client_id: "949805625842487456",
+        client_secret: "PSlmFAPK_iwVEM95e03Rw8dKR_qWA3B1",
         grant_type: "authorization_code",
-        code,
+        code: code.toString(),
         redirect_uri: "http://localhost:3001/api/auth/discord/redirect",
-      }
-    );
+      });
+      const response = await axios.post(
+        "https://discord.com/api/v8/oauth2/token",
+        formData.toString(),
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+      res.sendStatus(400);
+    }
   }
-  res.sendStatus(200);
 });
 
 app.listen(PORT, () => console.log(`Running on Port ${PORT}`));
